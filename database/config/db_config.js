ab17552f86b16ec2753f03287735b521af79d1da
Martin Grippo Palladino <37913298+mgrippo11@users.noreply.github.com>
2021-12-09 23:43:40 -0300

    Update db_config.js

--- database/config/db_config.js
+++ database/config/db_config.js
@@ -1,8 +1,8 @@
 module.exports = {
         database: {
             host: 'mysql-c1g1.alwaysdata.net',
-            user: 'c1g1',
-            password: 'Grupo01',
-            database: 'c1g1_empresa'
+            user: 'user',
+            password: 'password',
+            database: 'database'
         }
 };
